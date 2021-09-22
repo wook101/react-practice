1afa8b08b54ba94ea4c5cc6dfc857993c6cec2dd
wook101 <ehddnr1022@gmail.com>
2021-09-22 21:33:19 +0900

    깊은복사 함수 map,filter,slice,concat,spread 사용하기 수정

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ const list = [1,2,3]
 //JSX문법
 function App() {
   return <div>
-    <div>안녕하세요! {name} 입니다.</div>
-    <h1>나이는 {age}세 입니다.</h1>
-    <hr/>
-    <div>{list.map((n)=><h3>번호{n}</h3>)}</div>
+      <div>안녕하세요! {name} 입니다.</div>
+      <h1>나이는 {age}세 입니다.</h1>
+      <hr/>
+      <div>{list.map((n)=><h3>번호{n}</h3>)}</div>
     </div>
 }
 
