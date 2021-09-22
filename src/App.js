55687340401ad75827cdb9c104d6d97732e2b2bd
wook101 <ehddnr1022@gmail.com>
2021-09-22 19:34:42 +0900

    변수 let, 상수 const

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
 
+let name = "김동욱";
+const age = 29;
 
 //JSX문법
 function App() {
-  return <div>안녕하세여123455</div>
+  return <div>
+    <div>안녕하세요! {name} 입니다.</div>
+    <h1>나이는 {age}세 입니다.</h1>
+    <hr/>
+    </div>
 }
 
 export default App;
