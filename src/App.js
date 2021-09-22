a5417ae04301f0651e97b0afa3a4fbfb7b2e262f
wook101 <ehddnr1022@gmail.com>
2021-09-22 21:54:42 +0900

    object안에 값 수정하기

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './App.css';
 let name = "김동욱";
 const age = 29;
 const list = [1,2,3]
+
+//object수정하기
+const a={id:1,name:"홍길동",phone:"1111"}
+const b={...a, name:"김동욱"}
+console.log(b);
+
+
 //JSX문법
 function App() {
   return <div>
       <div>안녕하세요! {name} 입니다.</div>
       <h1>나이는 {age}세 입니다.</h1>
       <hr/>
-      <div>{list.map((n)=><h3>번호{n}</h3>)}</div>
+      <div>{list.map((n)=><h3 key={n}>번호{n}</h3>)}</div>
     </div>
 }
 
