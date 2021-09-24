3c3a62b0150d868426bf26f34348babbb650d1a7
wook101 <ehddnr1022@gmail.com>
2021-09-25 00:27:36 +0900

    useEffect사용하기

--- src/App2.js
+++ src/App2.js
@@ -3,19 +3,26 @@ import Sub from './Sub';
 import {num} from './Sub';
 //useEffect 실습
 const App2 = ()=> {
-
+  const [search,setSearch] = useState(0);
   const [data,setData] =  useState(0);
 
-  useEffect(()=>{ //실행시점: App2()함수가 최초 실행될때(마운트 될때), 첫번째 인자 callback
+
+  const download = ()=>{
+    let downloadData = 10;
+    setData(downloadData);
+  }
+
+  useEffect(()=>{ //실행시점: (1)App2()함수가 최초 실행될때(마운트 될때), (2)상태 변수가 변경될때(상태변수가 의존리스트에 등록되어 있을때), 첫번째 인자 callback, 두번째인자 dependency 의존리스트로 관리
     console.log("userEffect 실행됨!");
-  });
+    download();
+  },[search]);
 
 
   return <div>
       <h1>하이</h1>
+      <button onClick={()=>{setSearch()}}>검색하기</button><br></br>
       데이터: {data}
-      <Sub/>
-      서브 컴포넌트에서 가져온 num: {num}
+      <button onClick={()=>{setData(data+1)}}>더하기</button>
     </div>
 }
 
